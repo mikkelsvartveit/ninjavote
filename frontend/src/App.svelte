4ac67321f6cf67d22141dff95b17ad8dc56cce3c
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2022-05-24 12:51:02 +0200

    Set up real-time connection between Feathers server and Svelte app

---
+++ frontend/src/App.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import feathers from "@feathersjs/client";
+  import socketio from "@feathersjs/socketio-client";
+  import io from "socket.io-client";
+  import { onMount } from "svelte";
+
+  const socket = io("http://localhost:3030");
+  // @ts-ignore
+  const client = feathers();
+  client.configure(socketio(socket));
+  const messageService = client.service("messages");
+
+  let messages = [];
+  let newMessage = "";
+
+  client.service("messages").on("created", (message) => {
+    messages = [...messages, message];
+  });
+
+  const addMessage = () => {
+    if (newMessage.length > 0) {
+      messageService.create({ text: newMessage });
+      newMessage = "";
+    }
+  };
+
+  onMount(async () => {
+    messages = await messageService.find();
+    console.log(messages);
+  });
+
+  export let name: string;
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+
+  <input bind:value={newMessage} />
+  <button on:click={addMessage}>Add message</button>
+
+  <ul>
+    {#each messages as message}
+      <li>{message.text}</li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
