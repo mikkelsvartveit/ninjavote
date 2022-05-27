181f02a709bd4b91d23ce426c7ce732f8223495e
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2022-05-27 13:42:40 +0200

    Set up SvelteKit

---
+++ frontend/svelte.config.js
@@ -0,0 +1,22 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter(),
+		vite: {
+			resolve: {
+				alias: {
+					'xmlhttprequest-ssl': './node_modules/engine.io-client/lib/xmlhttprequest.js'
+				}
+			}
+		}
+	}
+};
+
+export default config;
