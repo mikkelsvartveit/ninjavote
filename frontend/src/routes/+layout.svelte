80fe7df0726bceef828242d2414c1feea10f4aae
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2023-01-04 23:39:46 +0100

    Fix some warnings

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,17 +1,5 @@
 <script>
   import "$lib/style/main.scss";
-  import LoadingIcon from "$lib/icons/loading.svg";
-  import CloseIcon from "$lib/icons/close.svg";
-  import CheckboxIcon from "$lib/icons/check.svg";
 </script>
 
-<svelte:head>
-  <link rel="preload" as="image" href={LoadingIcon} />
-  <link rel="preload" as="image" href={CloseIcon} />
-  <link rel="preload" as="image" href={CheckboxIcon} />
-</svelte:head>
-
 <slot />
-
-<style lang="scss">
-</style>
