1f3e154a8b23eef32c5ed9973eb7327e8af2e0d3
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2023-08-11 16:53:09 +0200

    Add meta description tag

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,4 +2,11 @@
   import "$lib/style/main.scss";
 </script>
 
+<svelte:head>
+  <meta
+    name="description"
+    content="Instantly create polls in your group chat with Ninjavote. Polls synchronize in real-time, and no signup is required!"
+  />
+</svelte:head>
+
 <slot />
